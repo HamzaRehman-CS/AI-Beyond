522e0a5ddbf5f04658fd07d3e4fc43eb3bc76660
Hamza Rehman <darkdeamon135@gmail.com>
2026-09-13 19:35:44 +0500

    Move scripts to js/ directory to eliminate serverless function misdetection

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-import * as THREE from './vendor/three.module.js';
-import {RoomEnvironment} from './vendor/RoomEnvironment.js';
+import * as THREE from '../vendor/three.module.js';
+import {RoomEnvironment} from '../vendor/RoomEnvironment.js';
 
 const $=(s,root=document)=>root.querySelector(s), $$=(s,root=document)=>[...root.querySelectorAll(s)];
 const clamp=(v,a=0,b=1)=>Math.max(a,Math.min(b,v));
